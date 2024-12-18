bb9f24450625e7ef6dac566045f52b36caa75e87
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2024-12-18 08:38:59 +0530

    adding my components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Mycomponents/Header';
+import Todos from './Mycomponents/Todos';
+import Footer from './Mycomponents/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Todos/>
+    <Footer/>
+    </>
   );
 }
 
---
+++ src/Mycomponents/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <div>
+            <nav id="navber">
+                <a className="contt" >Home</a>
+                <a className="contt" >Work</a>
+                <a className="contt" >About</a>
+                <a className="contt" >contact</a>
+            </nav>
+        </div>
+    )
+}
