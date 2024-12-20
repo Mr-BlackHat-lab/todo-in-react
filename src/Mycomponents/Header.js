372d65a01893dd44597e94a384524d294d9b5a80
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2024-12-20 08:50:06 +0530

    navbar complete

--- src/Mycomponents/Header.js
+++ src/Mycomponents/Header.js
@@ -1,14 +1,17 @@
 import React from "react";
+import "../cssOfComponents/header.css";
 
-export default function Header(){
-    return(
-        <div>
-            <nav id="navber">
-                <a className="contt" >Home</a>
-                <a className="contt" >Work</a>
-                <a className="contt" >About</a>
-                <a className="contt" >contact</a>
-            </nav>
-        </div>
-    )
+export default function Header() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><a>Home</a></li>
+          <li><a>Work</a></li>
+          <li><a>About</a></li>
+          <li><a>contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
